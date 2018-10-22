4578f1da5ba2fe6b1a0116a319a929967e87b834
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2018-10-22 19:11:48 +0500

    Minor Changes

--- index.js
+++ index.js
@@ -7,37 +7,7 @@ import { name as appName } from './app.json';
 
 //Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDUIONOy3lDirRL5fbh4GCkCIbFARzNEEU",
-    authDomain: "ibsesb-fyp.firebaseapp.com",
-    databaseURL: "https://ibsesb-fyp.firebaseio.com",
-    projectId: "ibsesb-fyp",
-    storageBucket: "ibsesb-fyp.appspot.com",
-    messagingSenderId: "162176471568"
 };
 firebase.initializeApp(config);
-
-// var serviceAccount = require('./src/credentials/serviceAccountKey.json');
-
-// firebase.initializeApp({
-//     credential: firebase.credential.cert(serviceAccount),
-//     databaseURL: "https://ibsesb-fyp.firebaseio.com"
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 AppRegistry.registerComponent(appName, () => AppRoutes);
